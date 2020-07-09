205dfd1dfb743e253ac7cc57616456fd48ac5ec5
Taras Oleksyn <prod.monitoring@sunpowercorp.com>
2020-07-09 10:09:03 +0300

    Update RozetkaTest.java

--- src/test/java/RozetkaTest.java
+++ src/test/java/RozetkaTest.java
@@ -35,14 +35,17 @@ public class RozetkaTest extends TestRunner {
     @Step("Verify logout from Rozetka site")
     void verifySuccessfulLogOut() {
         profilePage.logOut();
-
+        //TODO: test should end with assert
     }
 
     @Test
     @Step("Verify Search for samsung containing text in 10 first links")
     void verifyProductSearch() {
+        //TODO: re-do into ElementsCollection
         List<ElementsCollection> searchResultList = profilePage.searchProduct("Samsung").getListLink();
 
+        //TODO: searchResultList.texts().contains()
+        //TODO: move 'samsung' to variable
         assertTrue(searchResultList.toString().contains("Samsung"), "Text isn't correct");
         assertTrue(searchResultList.size() >= 10, "Not correct search result");
     }
