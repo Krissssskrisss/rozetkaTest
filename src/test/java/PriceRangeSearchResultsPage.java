ffacd7c57e192599e63ec926f0e575d0bdc8b534
Krissssskrisss <vasyliv1309@gmail.com>
2020-07-23 09:29:33 +0300

    alure

--- src/test/java/PriceRangeSearchResultsPage.java
+++ src/test/java/PriceRangeSearchResultsPage.java
@@ -1,17 +1,15 @@
-import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import java.util.Collections;
 import java.util.List;
 
-import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
 
-public class priceRangeSearchResultsPage {
+public class PriceRangeSearchResultsPage {
 
     @Step("Verify  product are in the correct price rang")
-    public List<SelenideElement> verifyPriceRangeOnThePage(){
+    public List<String> verifyPriceRangeOnThePage() {
+        return Collections.singletonList($x("//section[@class='content js_content content_type_catalog']").getText());
 
-        List<SelenideElement>  priceRangeSearchResults = $$x("//section[@class='content js_content content_type_catalog']");
-
-        return priceRangeSearchResults;
     }
 }
