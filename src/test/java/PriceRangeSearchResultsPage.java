481c1ffc2da2f5286b80029127d83b351323b9e5
Taras Oleksyn <prod.monitoring@sunpowercorp.com>
2020-07-23 11:40:58 +0300

    Update PriceRangeSearchResultsPage.java

--- src/test/java/PriceRangeSearchResultsPage.java
+++ src/test/java/PriceRangeSearchResultsPage.java
@@ -8,6 +8,8 @@ import static com.codeborne.selenide.Selenide.$x;
 public class PriceRangeSearchResultsPage {
 
     @Step("Verify  product are in the correct price rang")
+    //TODO: rename
+    //TODO: use Selenide $$x().texts();
     public List<String> verifyPriceRangeOnThePage() {
         return Collections.singletonList($x("//section[@class='content js_content content_type_catalog']").getText());
 
