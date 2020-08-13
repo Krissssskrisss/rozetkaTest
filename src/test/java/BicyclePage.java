6364a8c67df82dc7f36ea9cf7678d37d5be5bdfc
Taras Oleksyn <prod.monitoring@sunpowercorp.com>
2020-08-13 10:36:31 +0300

    Update BicyclePage.java

--- src/test/java/BicyclePage.java
+++ src/test/java/BicyclePage.java
@@ -6,9 +6,9 @@ import static com.codeborne.selenide.Selenide.$x;
 
 public class BicyclePage {
     @Step("Add two products to compare")
-    public ComparisonPage addTwoProductsForComparison(List<Bicycle> products) {
+    public ComparisonPage addTwoProductsForComparison(List<Bicycle> products) { //TODO: rename method or re-do
         for (Bicycle product : products) {
-            $x("//li[" + product.id + "]//app-compare-button[1]//button[1]").setValue(product.name);
+            $x("//li[" + product.id + "]//app-compare-button[1]//button[1]").setValue(product.name); //TODO: work with relative locators
         }
         $x("//a[@class='comparison-modal__link']").click();
 
